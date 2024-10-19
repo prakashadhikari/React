c7e6ad15139852f75e5f074280907e327f185b72
prakashadhikari <prakash.adhikari013@gmail.com>
2024-10-18 21:59:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
+import { createRoot } from "react-dom/client";
 
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="theme-color" content="#000000">
-	<!--
-      manifest.json provides metadata used when your web app is added to the
-      homescreen on Android. See https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifest/
-    -->
-	<link rel="manifest" href="%PUBLIC_URL%/manifest.json">
-	<link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
-	<!--
-      Notice the use of %PUBLIC_URL% in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `public` folder can be referenced from the HTML.
+const root = createRoot(document.getElementById("root"));
 
-      Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
-      work correctly both with client-side routing and a non-root public URL.
-      Learn how to configure a non-root public URL by running `npm run build`.
-    -->
-	<title>React App</title>
-</head>
+// Change the code below this line.
+const Card = (props) => (
+  <li>
+    <img src={props.link} alt={props.alt} />
+    <h3>{props.description}</h3>
+  </li>
+);
+// Change the code above this line.
 
-<body>
-	<noscript>
-		You need to enable JavaScript to run this app.
-	</noscript>
-	<div id="root"></div>
-	<!--
-      This HTML file is a template.
-      If you open it directly in the browser, you will see an empty page.
+// Change the code below this line.
+const App = () => (
+  <div>
+    <h1>Encyclopedia</h1>
+    <ul>
+      {/* Card 1 */}
+      <Card
+        link="https://codefinity-content-media.s3.eu-west-1.amazonaws.com/code-1/react/introduction-to-react/sun.png"
+        alt="Provide an appropriate description."
+        description="The sun shone brightly, casting a warm glow across the tranquil beach."
+      />
 
-      You can add webfonts, meta tags, or analytics to this file.
-      The build step will place the bundled scripts into the <body> tag.
+      {/* Card 2 */}
+      <Card
+        link="https://codefinity-content-media.s3.eu-west-1.amazonaws.com/code-1/react/introduction-to-react/mountain.png"
+        alt="Provide an appropriate description"
+        description="The hiker gazed in awe at the majestic mountain towering before them"
+      />
+    </ul>
+  </div>
+);
+// Change the code above this line.
 
-      To begin the development, run `npm start` or `yarn start`.
-      To create a production bundle, use `npm run build` or `yarn build`.
-    -->
-</body>
-
-</html>
+root.render(<App />);
